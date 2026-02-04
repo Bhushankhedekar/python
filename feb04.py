59caed8d0e0dff943943ca1829e30758aed567bc
Bhushan Dnyaneshwar Khedekar <148458560+Bhushankhedekar@users.noreply.github.com>
2026-02-04 17:45:33 +0530

    Add files via upload

---
+++ feb04.py
@@ -0,0 +1,31 @@
+# Data types in python 
+# Fundamental data types
+# 1. int
+# 2. float
+# 3. bool
+# 4. str
+# 5. complex
+# 6. string 
+
+# collective data types 
+# 1. list
+# 2. tuple
+# 3.dictionary
+# 4. set
+# 5. set
+# 6. frozenset
+# 7. range
+# 8. bytes
+# 9. bytearray
+# 10. none type
+
+# printing the id(memory address) of variables
+a = 23
+b = 22
+print(id(a))
+print(id(b)) 
+
+if a > b:
+    print("a will pay the bill")
+else:
+    print("b will pay the bill")
