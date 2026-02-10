ccd9aa4f28ba7f13f59db9355cfdd6b658e56475
Bhushan Dnyaneshwar Khedekar <148458560+Bhushankhedekar@users.noreply.github.com>
2026-02-10 10:03:39 +0530

    Add files via upload

---
+++ feb09.py
@@ -0,0 +1,16 @@
+# Task
+# create a variable and print its type and ID
+# create a variables 
+a = 23
+b = 22.59
+c = True
+
+#print the type pf variable 
+print(type(a))
+print(type(b))  
+print(type(c))
+
+# print the id of variable
+print(id(a))
+print(id(b)) 
+print(id(c))
