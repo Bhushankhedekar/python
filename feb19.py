15678560bc0a07f66412400f31277f6ca3d757b2
Bhushan Dnyaneshwar Khedekar <148458560+Bhushankhedekar@users.noreply.github.com>
2026-02-19 18:08:39 +0530

    Add files via upload

---
+++ feb19.py
@@ -0,0 +1,24 @@
+# String methods logical
+# String iterations 
+# for var_name in sequence:
+#    statement(s)
+# i = "Instagram"
+# for ch in i:
+#     print(i,"--->",i.index(ch)+1)
+
+# s = "Instagram"
+# for i in s:
+#     print(i,"--->",s.index(i))
+
+# s = "Instagram"
+# num = 1
+# for i in s:
+#     print(i,"--->",s.index(i)+1)
+#     num = num+1
+
+# s = "I love python programming"
+# count = 0
+# for i in s:
+#     if i =="p":
+#         count = count+1
+# print("Number of 'p's in the string is: ",count)
